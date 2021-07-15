ffb486fcc8ebf0ae4c9ba9054f5adfaf59d99245
Sasindu <sdilshara26@gmail.com>
2021-07-15 20:03:18 +0530

    finished build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Planet from "./components/Planet/Planet";
+import { planets } from "./Planet Details/PlanetsDts";
+import "./components/Styles.css";
+import nightSky from "./Static/night.jpg";
 
 function App() {
+  const [planet, setPlanet] = useState(planets[0]);
+  const [dropDown, setDropDown] = useState(false);
+
+  function handleNavClick(e) {
+    console.log(e.target.value);
+    const planetNo = e.target.value;
+    setPlanet(planets[planetNo]);
+  }
+
+  function handleDropDown() {
+    setDropDown(!dropDown);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src={nightSky} alt="" className="bg-img" />
+      <div className="nav-bar">
+        <h1>THE PLANETS</h1>
+        <div className="nav-planets">
+          <div className="drop-down">
+            <h3 className="drop-down-btn" onClick={handleDropDown}>
+              Planets
+            </h3>
+            <ul
+              className={`drop-down-menu ${dropDown && "drop-down-menu-show"}`}
+            >
+              {planets.map((planet) => {
+                return (
+                  <li
+                    className="nav-planet"
+                    id={planet.planetNumer}
+                    value={planet.planetNumer}
+                    onClick={(e) => {
+                      handleNavClick(e);
+                      handleDropDown();
+                    }}
+                  >
+                    {planet.planetName}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <ul className="main-nav">
+            {planets.map((planet) => {
+              return (
+                <li
+                  className="nav-planet"
+                  id={planet.planetNumer}
+                  value={planet.planetNumer}
+                  onClick={(e) => handleNavClick(e)}
+                >
+                  {planet.planetName}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <div className="planets">
+        <Planet plantDts={planet} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//Components
+//planet
+//Navbar
+//Image
+//Passage
+//Numerical Details
